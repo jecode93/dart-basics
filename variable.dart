f688639abd498bfb9a33d6ea50843e93f99f831b
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-06-11 22:37:17 -0400

    Type de variable

---
+++ variable.dart
@@ -0,0 +1,25 @@
+main() {
+//result show John
+  var name = 'John';
+  print(name);
+
+//result show Emmanuel
+  name = "Emmanuel";
+  print(name);
+
+  //integer
+  int age = 18;
+  print(age);
+
+  //double
+  double pi = 3.14;
+  print(pi);
+
+  //String
+  String nom = "Cadet";
+  print(nom);
+
+  //concatenation
+  print('Mon prénom est : $name et j\'ai $age ans. ');
+  print('Mon prénom est : ' + name);
+}
