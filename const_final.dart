5d80b6d91e5447ef40785b2b10a844b9e1415c2a
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-06-11 23:00:47 -0400

    Utilisation de const et final

---
+++ const_final.dart
@@ -0,0 +1,36 @@
+main() {
+  // Final et const ce sont des variables qu'on peut definit une seule fois, notamment le final.
+  const city = 'Brive'; // ou const String city, si on veut forcer le type.
+  const annee = 2019; // ou const int annee, si on veut forcer le type.
+
+  print(city);
+  print(annee);
+
+  final String hobbies = 'Musculation';
+  final String sport = 'Running';
+  final int vitesse = 15;
+  print(hobbies);
+  print(sport);
+
+  //Difference, si on veut redefinit le variable
+  city =
+      'Paris'; // Erreur parce qu'on ne peut pas changer une variable de type const
+
+  //result ok
+  int newYears = annee + 1;
+  print(newYears);
+
+  int newVitesse =
+      vitesse + 1; //Il est possible d'utiliser une variable de type const
+  final int multiplication = annee + 1;
+  print(newVitesse);
+
+  final int multiplication1 = annee + 2;
+
+  //Result error
+  const multiplication2 = vitesse + Une carrie;
+
+  //NB -> On ne peut pas utiliser une final dans une constant mais le contraire est possible.
+  // les valeurs de final ne peut pas changer dans une const.
+  // Le final peut utiliser partout, sauf dans les contantes
+}
