8e780e5d5f333a432bd25cb48890563606e1111f
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-06-14 15:31:38 -0400

    Working with switch

---
+++ switch.dart
@@ -0,0 +1,15 @@
+void main() {
+  String ville = 'Paris';
+
+  switch (ville) {
+    case 'Tulle':
+      print('Bienvenue a Tulle');
+      break;
+    case 'Paris':
+      print('Bienvenue a Paris');
+      break;
+    default:
+      print('Ville Inconnue');
+      break;
+  }
+}
