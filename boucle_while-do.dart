6c0c15b06ea423bc5371a9c08ffd2f626d56d001
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-06-14 16:24:14 -0400

    While & do-while loop.

---
+++ boucle_while-do.dart
@@ -0,0 +1,15 @@
+void main() {
+  int verre = 0;
+
+  //Using while loop
+  while (verre < 5) {
+    print('Toujours pas bourré');
+    verre++;
+  }
+
+  //Using do-while loop
+  do {
+    print('Toujours pas bourré');
+    verre++;
+  } while (verre < 5);
+}
